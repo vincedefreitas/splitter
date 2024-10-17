b02dfd4e59ebecdca468b99ffca1d5b21e15bca9
vincedefreitas <97405426+vincedefreitas@users.noreply.github.com>
2024-10-17 13:34:08 +0100

    Add amount prop to DisplayItem

--- src/components/CalculatorDisplay/CalculatorDisplay.jsx
+++ src/components/CalculatorDisplay/CalculatorDisplay.jsx
@@ -5,8 +5,8 @@ function CalculatorDisplay() {
   return (
     <section className="bg-cyan-800 w-6/12 h-full rounded-3xl p-10 flex flex-col justify-between">
       <div>
-        <DisplayItem displayText="Tip Amount" />
-        <DisplayItem displayText="Total" />
+        <DisplayItem displayText="Tip Amount" amount="0.00" />
+        <DisplayItem displayText="Total" amount="0.00" />
       </div>
       <ResetButton />
     </section>
--- src/components/CalculatorDisplay/DisplayItem.jsx
+++ src/components/CalculatorDisplay/DisplayItem.jsx
@@ -1,11 +1,11 @@
-function DisplayItem({ displayText }) {
+function DisplayItem({ displayText, amount }) {
   return (
     <div className="flex justify-between mb-10">
       <div>
         <p className="text-white">{displayText}</p>
         <p className="text-cyan-500">/ person</p>
       </div>
-      <p className="text-cyan-300 text-5xl">$0.00</p>
+      <p className="text-cyan-300 text-5xl">${amount}</p>
     </div>
   );
 }
