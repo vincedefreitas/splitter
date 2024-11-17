ddaa65e27debbe7a320f1a2b48eb978fa09195a3
vincedefreitas <97405426+vincedefreitas@users.noreply.github.com>
2024-11-17 21:16:49 +0000

    Add splitter title

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import Calculator from "./components/Calculator/Calculator";
 
 function App() {
   return (
-    <div className="lg:h-screen bg-cyan-100 flex justify-center items-center font-mono py-4">
+    <div className="lg:h-screen bg-cyan-100 flex flex-col justify-center items-center font-mono py-4">
+      <div class="mb-6 mt-2 lg:mb-20 text-4xl text-cyan-800 tracking-[.5em]">
+        <p>SPLI</p>
+        <p>TTER</p>
+      </div>
       <Calculator />
     </div>
   );
